92f03c52b5dcda1b33dcf04eef6dc9d90aea3c30
thaisc98 <thaisc98@hotmail.es>
2021-06-29 22:00:31 -0400

    [first] git first number and second number

--- src/main/java/Range.java
+++ src/main/java/Range.java
@@ -5,8 +5,29 @@ public class Range {
         this.range = s;
     }
 
+    // (3,6) = (3  = 3
+    private int getFirstNumber(String range){
+        String[] number = range.split(",");
+        String newValue = number[0]; // (3
+        String regex = newValue.replaceAll("[^0-9]", "");
+        return Integer.parseInt(regex);
+    }
+
+    private int getSecondNumber(String range){
+        String[] number = range.split(",");
+        String newValue = number[1]; //
+        String regex = newValue.replaceAll("[^0-9]", "");
+        return Integer.parseInt(regex);
+    }
 
     public boolean notContains(Range otherRange){
-        return false;
+        int firstNumber =  getFirstNumber(range);
+        int secondNumber =  getSecondNumber(range);
+
+        int firstOtherRange = getFirstNumber(otherRange.range);
+        int secondOtherRange =  getSecondNumber(otherRange.range);
+
+        return firstOtherRange < firstNumber || firstOtherRange > secondNumber;
+
     }
 }
--- src/test/java/RangeTest.java
+++ src/test/java/RangeTest.java
@@ -5,10 +5,18 @@ public class RangeTest {
 
     private String firstRange = "(3,6)";
 
-    @Test // (3 , 6 ) = (3,6)  no contains [7,10]
-    public void split_Range(){
+    @Test // (3 , 6 ) = (3,6) = [4,5]  no contains [7,10]
+    public void not_Cotains_Range_On_Other_Range(){
         Range r1 = new Range("(3,6)");
-        Range r2 = new Range("[7,10]");
+        Range r2 = new Range("[5,10]");
+        boolean actual = r1.notContains(r2);
+        Assert.assertTrue(actual);
+    }
+
+    @Test  // (3 , 6 ) = (3,6) = [4,5]  no contains [7,10]
+    public void not_Contains_Range_Is_False(){
+        Range r1 = new Range("(3,10)");
+        Range r2 = new Range("[5,8]");
         boolean actual = r1.notContains(r2);
         Assert.assertFalse(actual);
     }
